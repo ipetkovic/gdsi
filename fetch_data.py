4256e6196992d9582f6cca924743b80dc851bfe3
Ivo Petković <ivo.petkovic@gmail.com>
2017-03-17 23:11:01 +0100

    Form player and matches tables

--- fetch_data.py
+++ fetch_data.py
@@ -4,7 +4,8 @@ import sqlite3
 import re
 
 
-DATE_PATTERN = re.compile('Date\((\d)+\)')
+_DATE_PATTERN = re.compile('/Date\((\d+)\)/')
+
 
 def _get_database_name():
     return 'gdsi.db'
@@ -29,12 +30,11 @@ def _create_player_table(cursor):
     cursor.fetchone()
 
 
-def _get_players_data_from_site(soap_client, player_id_start):
+def _get_players_info(soap_client, player_id_start):
     return json.loads(soap_client.service.Igrac(player_id_start))
 
 
 def _update_player_table(db, soap_client):
-    import ipdb; ipdb.set_trace()
     cursor = db.cursor()
     if not _check_if_table_exists(cursor, 'players'):
         _create_player_table(cursor)
@@ -43,51 +43,96 @@ def _update_player_table(db, soap_client):
         result = _get_max_id(cursor, 'players')
         next_id = result + 1 if result else 0
 
-    players_data = _get_players_data_from_site(soap_client, next_id)
+    players_data = _get_players_info(soap_client, next_id)
     for player_data in players_data:
         cursor.execute('insert into players values(?, ?)',
                        (player_data['IgracId'], player_data['ImePrezime']))
-    import ipdb; ipdb.set_trace()
     db.commit()
 
 
 def _create_matches_table(cursor):
-    cursor.execute('create table matches '
-                   '(id integer primary key, season_id integer, '
-                   'league_id integer, date integer, '
-                   'quality_balls integer, location text, '
-                   'not_played integer,  report_id integer, '
-                   'reserved_id integer, '
-                   'foreign key(report_id) references players(id), '
-                   'foreign key(reserved_id) references players(id))')
+    cursor.execute(('create table matches (id integer primary key, '
+                    'league_id integer, season_id integer, winner_id integer, '
+                    'player1_id integer, player2_id integer, '
+                    'not_played integer, not_played_reason text, '
+                    'date integer, location text, '
+                    'reported_id integer, reserved_id integer, '
+                    'quality_balls integer, '
+                    'player1_set1 integer, player1_set2 integer, '
+                    'player1_set3 integer, player2_set1 integer, '
+                    'player2_set2 integer, player2_set3 integer, '
+                    'foreign key(winner_id) references players(id), '
+                    'foreign key(player1_id) references players(id), '
+                    'foreign key(player2_id) references players(id), '
+                    'foreign key(reported_id) references players(id), '
+                    'foreign key(reserved_id) references players(id))'))
     cursor.fetchone()
 
 
-def _get_matches_data_partial(soap_client, match_id_start):
-    return json.loads(soap_client.service.SusretJSON(match_id_start))
+def _get_match_info_partial(client, start_id):
+    match_info = {
+        'metadata': json.loads(client.service.SusretJSON(start_id - 1)),
+        'result': json.loads(client.service.SusretIgracJSON(start_id - 1))
+    }
+    return match_info
 
 
-def _get_matches_data_from_site(soap_client, match_id_start):
-    partial_data = _get_matches_data_partial(soap_client, match_id_start)
-    match_data = []
-    while (partial_data):
-        match_data += partial_data
-        next_id = match_data[-1]['SusretId'] + 1
-        partial_data = _get_matches_data_partial(soap_client, next_id)
+def _form_match_data(metadata, players):
+    result = re.match(_DATE_PATTERN, metadata['Datum'])
+    assert(result)
+    date = int(result.group(1))
+    if players[0]['Pobjednik']:
+        winner_id = players[0]['IgracId']
+    else:
+        winner_id = players[1]['IgracId']
+    if metadata['DomacinLoptice'] is not None:
+        quality_balls = metadata['DomacinLoptice']
+    else:
+        quality_balls = None
+
+    match_data = (
+        metadata['SusretId'],
+        metadata['LigaId'],
+        metadata['SezonaId'],
+        winner_id,
+        players[0]['IgracId'],
+        players[1]['IgracId'],
+        int(metadata['NijeOdigran']),
+        metadata['RazlogNeigranja'],
+        date,
+        metadata['Lokacija'],
+        metadata['PrijavioId'],
+        metadata['TkoJeRezervirao'],
+        quality_balls,
+        players[0]['Set1'],
+        players[0]['Set2'],
+        players[0]['Set3'],
+        players[1]['Set1'],
+        players[1]['Set2'],
+        players[1]['Set3']
+    )
 
     return match_data
 
-        
-        for match_data in matches_data:
-            pass
-        match_data = map(lambda x: (x['SusretId'], x['SezonaId'], x['LigaId'], x['Datum'], x['DomacinLoptice'], x['Lokacija'], x['NijeOdigran'], x['PrijavioId'], x['TkoJeRezervirao']), match)
-        matches_data = _get_matches_data_partial(soap_client, match_id_start)
 
-    return 
+def _get_match_data(client, match_id_start):
+    match_info = _get_match_info_partial(client, match_id_start)
+    while len(match_info['metadata']):
+        match_num = len(match_info['metadata'])
+        assert(len(match_info['result']) == 2 * match_num)
+        for idx in xrange(match_num):
+            metadata = match_info['metadata'][idx]
+            players = match_info['result'][2*idx:2*(idx + 2)]
+            assert (metadata['SusretId'] == players[0]['SusretId'])
+            assert (metadata['SusretId'] == players[1]['SusretId'])
+            match_data = _form_match_data(metadata, players)
+            yield _form_match_data(metadata, players)
+
+        next_id = match_data[0] + 1
+        match_info = _get_match_info_partial(client, next_id)
 
 
-def _update_matches_table(db, soap_client):
-    import ipdb; ipdb.set_trace()
+def _update_matches_table(db, client):
     cursor = db.cursor()
     if not _check_if_table_exists(cursor, 'matches'):
         _create_matches_table(cursor)
@@ -95,19 +140,11 @@ def _update_matches_table(db, soap_client):
     else:
         result = _get_max_id(cursor, 'matches')
         next_id = result + 1 if result else 0
+    insert_query = 'insert into matches values({})'.format(','.join('?' * 19))
+    for data in _get_match_data(client, next_id):
+        cursor.execute(insert_query, data)
+    db.commit()
 
-    matches_data = _get_matches_data_from_site(soap_client, next_id)
-    for match_data in matches_data:
-
-    result = re.match(DATE_PATTERN, match_data['Datum'])
-    assert(result)
-    date = result.groups()[0]
-    row = (match_data['SusretId'], match_data['SezonaId'],
-           match_data['LigaId'], date,
-           int(match_data['DomacinLoptice'] == 'Da'),
-           match_data['Lokacija'], match_data['NijeOdigran'],
-           match_data['PrijavioId'], match_data['TkoJeRezervirao'])
-    cursor.execute('insert into matches values(?)', row)
 
 def run():
     wsdl = 'http://tenisliga.com/ws/public.asmx?WSDL'
